a1c555e4fb51928ea031053c3e64daddbd71482f
fuqiang <fuqiang@ejoy.com>
2017-09-11 16:16:38 +0800

    add README.md

--- init.sh
+++ init.sh
@@ -1,1 +1,8 @@
+echo -e '\033[91m--install python-pygame--\033[0m'
+sudo apt-get install python-pygame
+echo -e '\033[91m--link server file to skynet/--\033[0m'
 ln -fs $(pwd)/server $(pwd)/skynet/
+echo -e '\033[91m--install pysproto--\033[0m'
+make pysproto
+echo -e '\033[91m--compile skynet && compile sproto--\033[0m'
+make
