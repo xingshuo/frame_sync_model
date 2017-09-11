93cc8b5014ef6b89553eb393ef02e9794d0b7498
fuqiang <fuqiang@ejoy.com>
2017-09-11 19:01:06 +0800

    ..

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 echo -e '\033[91m--install python-pygame--\033[0m'
 sudo apt-get install python-pygame
 echo -e '\033[91m--git clone jemalloc to skynet/--\033[0m'
-cd skynet/3rd && git clone https://github.com/jemalloc/jemalloc.git && cd ../../
+cd skynet/3rd && git clone https://github.com/jemalloc/jemalloc.git
+cd ../../
 echo -e '\033[91m--link server file to skynet/--\033[0m'
 ln -fs $(pwd)/server $(pwd)/skynet/
 echo -e '\033[91m--install pysproto--\033[0m'
