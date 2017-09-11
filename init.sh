9fea380d31564df9db535079f71300cb6f8fb5c2
fuqiang <fuqiang@ejoy.com>
2017-09-11 18:56:25 +0800

    .

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ cd skynet/3rd && git clone https://github.com/jemalloc/jemalloc.git && cd ../../
 echo -e '\033[91m--link server file to skynet/--\033[0m'
 ln -fs $(pwd)/server $(pwd)/skynet/
 echo -e '\033[91m--install pysproto--\033[0m'
+sudo pip install Cython
 make pysproto
 echo -e '\033[91m--compile skynet && compile sproto--\033[0m'
 make
