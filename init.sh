6414a80a2b3a2f02858290be2f382a3e0940583e
xingshuo <leavethewindoff@163.com>
2017-09-13 21:51:04 +0800

    .

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 echo -e '\033[91m--install python-pygame--\033[0m'
 sudo apt-get install python-pygame
+echo -e '\033[91m--install autoconf--\033[0m'
+sudo apt-get install autoconf
+echo -e '\033[91m--install readline--\033[0m'
+sudo apt-get install libreadline-dev
+echo -e '\033[91m--install python-setuptools--\033[0m'
+sudo apt-get install python-setuptools
 echo -e '\033[91m--git clone jemalloc to skynet/--\033[0m'
 cd skynet/3rd && git clone https://github.com/jemalloc/jemalloc.git
 cd ../../
